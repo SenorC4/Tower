ae301af1738297ce2aec6f39da55edcc01129d2e
Luke LeCain <44325992+SenorC4@users.noreply.github.com>
2019-08-28 15:29:31 -0700

    pause menu
    its a pause menu

---
+++ Assets/scripts/pauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool GameisPaused = false;
+    public GameObject menu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (GameisPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+           
+            
+        }
+    }
+
+    public void Resume()
+    {
+        menu.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPaused = false; ;
+    }
+
+    public void Pause()
+    {
+        menu.SetActive(true);
+        Time.timeScale = 0f;
+        GameisPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+        
+        Debug.Log("load main menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
