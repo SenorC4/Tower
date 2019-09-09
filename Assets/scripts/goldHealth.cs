62c9154f034475cfcf926f8f5f9df7d0221c7b2a
Luke LeCain <44325992+SenorC4@users.noreply.github.com>
2019-09-08 19:06:49 -0700

    health bar
    made a health bar for gold block

--- Assets/scripts/goldHealth.cs
+++ Assets/scripts/goldHealth.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class goldHealth : MonoBehaviour
 {
-    public int health = 200;
+    public float health = 200f;
     public GameObject GameOver;
+
+    public Image healthBar;
 
     public void TakeDamage()
     {
         health -= 10;
+        healthBar.fillAmount = health / 200f;
 
         if(health <= 0)
         {
